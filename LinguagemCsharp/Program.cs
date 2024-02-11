a0165747bdfe1b95710e613749d92ba476fd0759
Pamela Rodrigues Louzada <pamela.rlouzada@gmail.com>
2024-02-10 21:01:38 -0300

    Redimensiona as dimensões de uma imagem

--- LinguagemCsharp/Program.cs
+++ LinguagemCsharp/Program.cs
@@ -1,29 +1,82 @@
 ﻿
+using System.Drawing;
 
-using LinguagemCsharp;
+Console.WriteLine("Iniciando redimensionador");
+Thread thread = new Thread(Redimensionar);
+thread.Start();
 
-ContaBanco contaB = new ContaBanco("Pâmela", 10000);
-Console.WriteLine($"A conta {contaB.Numero} de {contaB.Titular} foi criada com saldo de {contaB.Saldo}");
 
-contaB.Depositar(800, DateTime.Now, "Recebi um pgto");
-Console.WriteLine($"A conta {contaB.Numero} de {contaB.Titular} está com saldo de {contaB.Saldo}");
-
-try
+static void Redimensionar()
 {
-    contaB.Sacar(10000, DateTime.Now, "Fiz um pgto");
-    Console.WriteLine($"A conta {contaB.Numero} de {contaB.Titular} está com saldo de {contaB.Saldo}");
-}
-catch (ArgumentOutOfRangeException ex)
-{
-    Console.WriteLine(ex.Message );
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Operação não realizada");
-}
+    #region "Diretórios"
+    string diretorioEntrada = "Arquivos_Entradas";
+    string diretorioFinalizado = "Arquivos_Finalizados";
+    string diretorioRedimensionado = "Arquivos_Redimensionados";
 
-Console.WriteLine(contaB.PegarMovimentacoes());
+    if (!Directory.Exists(diretorioEntrada))
+    {
+        Directory.CreateDirectory(diretorioEntrada);
+    }
+    if (!Directory.Exists(diretorioFinalizado))
+    {
+        Directory.CreateDirectory(diretorioFinalizado);
+    }
+    if (!Directory.Exists(diretorioRedimensionado))
+    {
+        Directory.CreateDirectory(diretorioRedimensionado);
+    }
+    #endregion
 
+    FileStream fileStream;
+    FileInfo fileInfo;
+    while (true)
+    {
+        // meu programa vai olhar para pasta de entrada
+        // se tiver arquivo, ele irá redimensionar
+        var arquivosEntrada = Directory.EnumerateFiles(diretorioEntrada);
+
+        // ler o tamanho que irá redimensionar
+        int novaAltura = 200;
+
+        foreach (var arquivo in arquivosEntrada)
+        {
+            fileStream = new FileStream(arquivo, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            fileInfo = new FileInfo(arquivo);
+
+            string caminho = Environment.CurrentDirectory + @"\" + diretorioRedimensionado + @"\" +
+                            DateTime.Now.Millisecond.ToString() + "_" + fileInfo.Name ;
+
+            //Console.WriteLine(file);
+
+            // Redimensiona
+            Redimensionador(Image.FromStream(fileStream), novaAltura, caminho);
+            fileStream.Close();
+
+            string caminhoFinalizado = Environment.CurrentDirectory + @"\" + diretorioFinalizado 
+                + @"\" + fileInfo.Name;
+
+            // move arquivos de entrada para finalizados
+            fileInfo.MoveTo(caminhoFinalizado);
+        }
+
+        Thread.Sleep(new TimeSpan(0, 0, 2));
+    }
+
+    static void Redimensionador(Image imagem, int altura, string caminho)
+    {
+        double ratio = (double)altura/imagem.Height;
+        int novaLargura = (int)(imagem.Width * ratio);
+        int novaAltura = (int)(imagem.Height * ratio);
+
+        Bitmap novaImagem = new Bitmap(novaLargura, novaAltura);
+        using(Graphics g = Graphics.FromImage(novaImagem))
+        {
+            g.DrawImage(imagem, 0, 0, novaLargura, novaAltura);
+        }
+        novaImagem.Save(caminho);
+        imagem.Dispose();
+    } 
+}
 
 
 
