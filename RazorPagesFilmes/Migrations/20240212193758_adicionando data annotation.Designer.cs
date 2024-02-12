217e8bcd64af3d80b545c9b442c643eebd2ba07a
Pamela Rodrigues Louzada <pamela.rlouzada@gmail.com>
2024-02-12 16:45:41 -0300

    Insere js para correção de preço

--- RazorPagesFilmes/Migrations/20240212193758_adicionando data annotation.Designer.cs
+++ RazorPagesFilmes/Migrations/20240212193758_adicionando data annotation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesFilmes.Data;
 
@@ -11,9 +12,10 @@ using RazorPagesFilmes.Data;
 namespace RazorPagesFilmes.Migrations
 {
     [DbContext(typeof(RazorPagesFilmesContext))]
-    partial class RazorPagesFilmesContextModelSnapshot : ModelSnapshot
+    [Migration("20240212193758_adicionando data annotation")]
+    partial class adicionandodataannotation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RazorPagesFilmes/Models/Filme.cs
+++ RazorPagesFilmes/Models/Filme.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 
 namespace RazorPagesFilmes.Models
@@ -6,15 +7,26 @@ namespace RazorPagesFilmes.Models
     public class Filme
     {
         public int Id { get; set; }
+
+        [StringLength(100, MinimumLength =3)]
+        [MaxLength(100, ErrorMessage ="errou")]
         public string Titulo { get; set; } = string.Empty;
+
         [Display(Name ="Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime DataLancamento { get; set; }
+
+        [Required(ErrorMessage ="Digite o gênero do filme")]
+        [StringLength(30, MinimumLength = 3)]
         [Display(Name = "Gênero")]
         public string Genero { get; set; } = string.Empty;
-        [Display(Name = "Preço")]
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Preco { get; set; }
+
         [Display(Name = "Avaliação")]
+        [Range(0,10)]
         public int Avaliacao { get; set; } = 0;
     }
 }
